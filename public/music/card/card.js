b92cfbc94ee1b57328145e7536a2615af4bc921e
JULIOCANCHALA <juliofernando1c@hotmail.com>
2018-01-18 16:43:01 -0500

    Ready front and back faces of card with rotating

--- public/music/card/card.js
+++ public/music/card/card.js
@@ -5,7 +5,7 @@ export class cardMusic {
     }
 
     templateCard() {
-         return (
+/*          return (
             `<li class="music__card">
             <div class="music__card-info">
                     <h2 class="music__card-name">${this.dataCard.name}</h2>
@@ -14,21 +14,42 @@ export class cardMusic {
             <img src="https://www.tenvinilo.com/vinilos-decorativos/img/preview/adhesivos-discos-vinilo-personalizados-5655.png" class="music__card-disc" alt="disc image">
             <img src="${this.dataCard.imgUrl}" class="music__card-img" alt="card image">
         </li>`
-        )        
-/*         return (
+        )  */       
+        return (
             `<li class="music__card">
-            <div class="music__card--front">
-                <div class="music__card-info">
-                    <h2 class="music__card-name">${this.dataCard.name}</h2>
-                    <h2 class="music__card-category">${this.dataCard.category}</h2>
+            <div class="music__card-container">
+                <div class="music__card-front">
+                    <div class="music__card-info">
+                        <div class="music__card-text">
+                            <h2 class="music__card-name">${this.dataCard.name}</h2>
+                            <h2 class="music__card-category">${this.dataCard.category}</h2>
+                    </div>     
+                        <img src="https://www.tenvinilo.com/vinilos-decorativos/img/preview/adhesivos-discos-vinilo-personalizados-5655.png" class="music__card-disc" alt="disc image">
+                        <img src="${this.dataCard.imgUrl}" class="music__card-img" alt="card image">
+                    </div>              
                 </div>
-                <img src="https://www.tenvinilo.com/vinilos-decorativos/img/preview/adhesivos-discos-vinilo-personalizados-5655.png" class="music__card-disc" alt="disc image">
-                <img src="${this.dataCard.imgUrl}" class="music__card-img" alt="card image">
-            </div>
-            <div class="music__card--back">
+                <div class="music__card-back">
+                    <h2 class="music__card-back-summary">${this.dataCard.info}</h2>
+
+                    <div class="music__card-back-contentInfo">
+                        <h2 class="music__card-back-title">Origen: </h2>
+                        <h2 class="music__card-back-data">${this.dataCard.origin}</h2>
+                    </div>
+
+                    <div class="music__card-back-contentInfo">
+                        <h2 class="music__card-back-title">Actividad: </h2>
+                        <h2 class="music__card-back-data">${this.dataCard.yearsActive}</h2>
+                    </div>
+
+                    <div class="music__card-back-contentInfo">
+                        <h2 class="music__card-back-title">Miembros: </h2>
+                        <h2 class="music__card-back-data">${this.dataCard.members}</h2>
+                    </div>
+
+                </div>
             </div>
         </li>`
-        ) */
+        )
     }
 
 
